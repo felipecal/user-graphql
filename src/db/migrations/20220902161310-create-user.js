4794fcb467fb9d27a0c438e82841a9c065e3869f
Felipe <fcalldas@gmail.com>
2022-09-02 15:58:35 -0300

    🐛 fix a bug in migrations

--- src/db/migrations/20220902161310-create-user.js
+++ src/db/migrations/20220902161310-create-user.js
@@ -25,11 +25,11 @@ module.exports = {
         allowNull: false
       },
       createdAt: {
-        allowNull: false,
+        allowNull: true,
         type: Sequelize.DATE
       },
       updatedAt: {
-        allowNull: false,
+        allowNull: true,
         type: Sequelize.DATE
       },
       removed_at: {
