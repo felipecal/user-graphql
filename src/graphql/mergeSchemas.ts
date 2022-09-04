08072b9b58062415ce0d407ba11867a703ee9067
Felipe <fcalldas@gmail.com>
2022-09-04 12:26:26 -0300

    🚚 renaming path

--- src/graphql/mergeSchemas.ts
+++ src/graphql/mergeSchemas.ts
@@ -2,7 +2,7 @@ import { mergeTypeDefs, mergeResolvers } from '@graphql-tools/merge';
 import { loadFilesSync } from '@graphql-tools/load-files';
 import path from 'path';
 
-const typesArray = loadFilesSync(path.join(__dirname, 'typeDefs', '**'));
+const typesArray = loadFilesSync(path.join(__dirname, 'schemas', '**'));
 const typeDefs = mergeTypeDefs(typesArray);
 
 const resolversArray = loadFilesSync(path.join(__dirname, 'resolvers', '**'))
