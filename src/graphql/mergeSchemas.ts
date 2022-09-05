22a4d88ee903dd1bbed1b9731e3c069ccc0cb4bb
Felipe <fcalldas@gmail.com>
2022-09-05 13:49:20 -0300

    🐛 fixxing a bug

--- src/graphql/mergeSchemas.ts
+++ src/graphql/mergeSchemas.ts
@@ -1,11 +1,12 @@
 import { mergeTypeDefs, mergeResolvers } from '@graphql-tools/merge';
 import { loadFilesSync } from '@graphql-tools/load-files';
+
 import path from 'path';
 
-const typesArray = loadFilesSync(path.join(__dirname, 'schemas', '**'));
+const typesArray = loadFilesSync(path.join(__dirname, 'modules', 'schemas', '**'));
 const typeDefs = mergeTypeDefs(typesArray);
 
-const resolversArray = loadFilesSync(path.join(__dirname, 'resolvers', '**'))
+const resolversArray = loadFilesSync(path.join(__dirname, 'modules', 'resolvers', '**'));
 const resolvers = mergeResolvers(resolversArray) as any;
 
 export { typeDefs, resolvers };
