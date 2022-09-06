f119e37ab5aa9c930174e9773cf10df429abc980
Felipe <fcalldas@gmail.com>
2022-09-06 12:32:51 -0300

    🌱 adding seeders

---
+++ src/db/seeders/20220905173855-post.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+      await queryInterface.bulkInsert('post', [{
+        title: 'Clean Code',
+        description: 'just a simple book about how to improve your code and the style how you write.',
+        created_at: new Date(),
+        active: true,
+        user_id: 1
+      },
+    {
+      title: 'Clean Architeture',
+      description: 'A book about how to improve your architecture.',
+      created_at: new Date(),
+      active: true,
+      user_id: 2
+    }], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+      await queryInterface.bulkDelete('post', null, {});
+  }
+};
