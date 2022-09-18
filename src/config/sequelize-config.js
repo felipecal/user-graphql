26393810cdbb8b27a419cd58b85b04f3471b7c7d
Caldas <fcalldas@gmail.com>
2022-09-18 12:43:13 -0300

    🐛 fixxing a bug in enviroment of password

--- src/config/sequelize-config.js
+++ src/config/sequelize-config.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const host = process.env.DB_HOST || '127.0.0.1';
 const port = parseInt(process.env.DB_PORT|| 5432);
 const username = process.env.DB_USERNAME || 'postgres';
-const password = process.env.DB_PASSWORD || 1234; 
+const password = process.env.DB_PASSWORD || '1234'; 
 const database = process.env.DB_DATABASE || 'user_ts';
 const dialect = 'postgres';
 
