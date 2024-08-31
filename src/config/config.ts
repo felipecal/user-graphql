feaa36b26ffe752159d68bab9541e32f09033f86
Felipe <fcalldas@gmail.com>
2024-08-31 12:01:02 -0300

    🐛 fix: fix a bug

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,19 +1,12 @@
 import { Sequelize } from 'sequelize-typescript';
-import { config as dotEnv } from 'dotenv';
-
-dotEnv();
-
-const env = process.env.NODE_ENV;
-const dialect = env === 'test' ? 'sqlite' : 'postgres';
 
 export const sequelize = new Sequelize({
     host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT || "5432") ,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    storage: './tests/database.sqlite',
-    dialect,
+    port: parseInt(process.env.DB_PORT || "5432"),
+    username: process.env.DB_USERNAME || "postgres",
+    password: process.env.DB_PASSWORD || "postgres",
+    database: process.env.DB_DATABASE || "postgres",
+    dialect: "postgres",
     define: {
         timestamps: true,
         underscored: true
