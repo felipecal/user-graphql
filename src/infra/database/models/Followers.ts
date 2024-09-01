bce6b72b722e039f87b48ed771ef8a6b1224c0e0
Felipe <fcalldas@gmail.com>
2024-09-01 16:55:38 -0300

    ✨ feat: add new models

---
+++ src/infra/database/models/Followers.ts
@@ -0,0 +1,82 @@
+import { DataTypes, Model } from 'sequelize';
+import { sequelize } from './index';
+import UserModel from './Users';
+import PostModel from './Posts';
+
+class FollowersModel extends Model {
+}
+
+
+FollowersModel.init(
+    {
+        id: {
+            type: DataTypes.STRING,
+            defaultValue: DataTypes.UUIDV4,
+            allowNull: false,
+            primaryKey: true
+        },
+        user_follow: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            references: {
+                model: UserModel,
+                key: 'id'
+            }
+        },
+        user_followed: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            references: {
+                model: UserModel,
+                key: 'id'
+            }
+        },
+        created_at: {
+            type: DataTypes.DATE,
+            defaultValue: new Date(),
+            allowNull: true
+        },
+        updated_at: {
+            type: DataTypes.DATE,
+            defaultValue: new Date(),
+            allowNull: true
+        },
+        removed_at: {
+            type: DataTypes.DATE,
+            allowNull: true
+        }
+    },
+    {
+        timestamps: true,
+        freezeTableName: true,
+        tableName: 'followers',
+        sequelize,
+        underscored: true,
+        paranoid: true,
+        deletedAt: 'removed_at'
+    },
+);
+
+UserModel.hasMany(FollowersModel, {
+    foreignKey: 'user_follow',
+    sourceKey: 'id',
+    as: 'following'
+});
+
+UserModel.hasMany(FollowersModel, {
+    foreignKey: 'user_followed',
+    sourceKey: 'id',
+    as: 'followers'
+});
+
+FollowersModel.belongsTo(UserModel, {
+    foreignKey: 'user_follow',
+    as: 'follower'
+});
+
+FollowersModel.belongsTo(UserModel, {
+    foreignKey: 'user_followed',
+    as: 'followed'
+})
+
+export default FollowersModel;
---
+++ src/infra/database/models/Messages.ts
@@ -0,0 +1,80 @@
+import { DataTypes, Model } from 'sequelize';
+import { sequelize } from './index';
+import UserModel from './Users';
+import PostModel from './Posts';
+
+class MessageModel extends Model {
+}
+
+
+MessageModel.init(
+    {
+        id: {
+            type: DataTypes.STRING,
+            defaultValue: DataTypes.UUIDV4,
+            allowNull: false,
+            primaryKey: true
+        },
+        post_id: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            references: {
+                model: PostModel,
+                key: 'id'
+            }
+        },
+        user_id: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            references: {
+                model: UserModel,
+                key: 'id'
+            }
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        created_at: {
+            type: DataTypes.DATE,
+            defaultValue: new Date(),
+            allowNull: true
+        },
+        updated_at: {
+            type: DataTypes.DATE,
+            defaultValue: new Date(),
+            allowNull: true
+        },
+        removed_at: {
+            type: DataTypes.DATE,
+            allowNull: true
+        }
+    },
+    {
+        timestamps: true,
+        freezeTableName: true,
+        tableName: 'messages',
+        sequelize,
+        underscored: true,
+        paranoid: true,
+        deletedAt: 'removed_at'
+    },
+);
+
+PostModel.hasMany(MessageModel, {
+    foreignKey: 'post_id',
+    sourceKey: 'id',
+    as: 'post_messages'
+});
+
+MessageModel.belongsTo(PostModel, {
+    foreignKey: 'post_id',
+    as: 'post'
+})
+
+MessageModel.belongsTo(UserModel, {
+    foreignKey: 'user_id',
+    as: 'user'
+});
+
+export default MessageModel;
